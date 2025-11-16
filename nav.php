bb5f7ed7859f75824976f55677f0ac01a6f28554
hugo <hugo.valente7@etu.univ-lorraine.fr>
2025-11-16 13:58:57 +0100

    navbar

---
+++ nav.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav style="border: 1px solid black; display: inline ; padding: 10px">
+        <button type="button">Navigation</button>
+        <button type="button">Recette <img src="Photos/Coeur_plein.png" width="15px" height="15px"/></button>
+        Recherche: 
+        <form style="display: inline;" action="" method="POST">
+            <input type="search" />
+            <button type="submit"><img src="Photos/Loupe.png" width="15px" height="15px"/></button>
+        </form>
+        <button>Zone de connexion</button>
+    </nav>
+    <aside>
+
+    </aside>
+    <main>
+        
+    </main>
+</body>
+</html>
