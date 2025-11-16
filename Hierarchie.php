44bc10fe45e2cc868723cbd666da35942c8c4d22
hugo <hugo.valente7@etu.univ-lorraine.fr>
2025-11-16 14:18:52 +0100

    hierarchie statique

---
+++ Hierarchie.php
@@ -0,0 +1,6 @@
+<aside style="border: 1px solid black; padding : 10px;">
+    <h2>Aliment courant</h2>
+    <p id="Fil d'Ariane"></p>
+    <h3>Sous-catégories :</h3>
+    <ul id="hierarchie"></ul>
+</aside>
--- nav.php
+++ nav.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    function ouvrirHierarchie() {
+        
+    }
 </head>
 <body>
     <nav style="border: 1px solid black; display: inline ; padding: 10px">
-        <button type="button">Navigation</button>
+        <button type="button" onClick="ouvrirHierarchie()">Navigation</button>
         <button type="button">Recette <img src="Photos/Coeur_plein.png" width="15px" height="15px"/></button>
         Recherche: 
         <form style="display: inline;" action="" method="POST">
@@ -17,7 +21,7 @@
         <button>Zone de connexion</button>
     </nav>
     <aside>
-
+    
     </aside>
     <main>
         
